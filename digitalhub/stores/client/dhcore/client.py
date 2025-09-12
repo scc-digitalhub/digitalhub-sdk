fca66eda8d8fafd3dba52fb374acdd0ae3cadd25
EvangMM <matteomartini92@yahoo.it>
2025-09-12 15:24:57 +0200

    chore: fixed small things

--- digitalhub/stores/client/dhcore/client.py
+++ digitalhub/stores/client/dhcore/client.py
@@ -26,8 +26,8 @@ if typing.TYPE_CHECKING:
 
 # API levels that are supported
 MAX_API_LEVEL = 20
-MIN_API_LEVEL = 11
-LIB_VERSION = 13
+MIN_API_LEVEL = 14
+LIB_VERSION = 14
 
 
 class ClientDHCore(Client):
