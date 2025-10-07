1057a2218eb1b25b1cd8308189ced2f353ade0f2
Matteo Martini <47110912+EvangMM@users.noreply.github.com>
2025-10-07 14:23:02 +0200

    fix: fixed injection of index=False

--- digitalhub/stores/readers/data/pandas/reader.py
+++ digitalhub/stores/readers/data/pandas/reader.py
@@ -133,7 +133,9 @@ class DataframeReaderPandas(DataframeReader):
         -------
         None
         """
-        df.to_csv(dst, index=False, **kwargs)
+        if "index" not in kwargs:
+            kwargs["index"] = False
+        df.to_csv(dst, **kwargs)
 
     @staticmethod
     def write_parquet(df: pd.DataFrame, dst: str | BytesIO, **kwargs) -> None:
@@ -153,7 +155,9 @@ class DataframeReaderPandas(DataframeReader):
         -------
         None
         """
-        df.to_parquet(dst, index=False, **kwargs)
+        if "index" not in kwargs:
+            kwargs["index"] = False
+        df.to_parquet(dst, **kwargs)
 
     @staticmethod
     def write_table(df: pd.DataFrame, table: str, engine: Any, schema: str | None = None, **kwargs) -> None:
@@ -177,7 +181,9 @@ class DataframeReaderPandas(DataframeReader):
         -------
         None
         """
-        df.to_sql(table, engine, schema=schema, index=False, **kwargs)
+        if "index" not in kwargs:
+            kwargs["index"] = False
+        df.to_sql(table, engine, schema=schema, **kwargs)
 
     ##############################
     # Utils
